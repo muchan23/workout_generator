a913bc44f3205eacf5c0cd0bd960e93a0d62b3f6
muchan23 <murakami-yuki866@g.ecc.u-tokyo.ac.jp>
2025-09-04 23:50:54 +0900

    update components

--- src/components/WorkoutTimer.tsx
+++ src/components/WorkoutTimer.tsx
@@ -1,2 +1,9 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import { WorkoutMenu, Exercise } from '../lib/types';
+
+interface WorkoutTimerProps {
+    workout: WorkoutMenu;
+    onCompliete: () => void;
+    onPause: () => void;
+    onResume: () => void;
+}
